58bc665fa227a16c5a2862d47510e6c5ab0e175c
Your Name <you@example.com>
2025-09-24 15:17:31 +0530

    add collectstatic

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,11 @@
 
-from django.conf import settings
+
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:slug>/', views.job_detail, name='job_detail'),
     path('category/<int:category_id>/', views.jobs_by_category, name='jobs_by_category'),
 ]
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
